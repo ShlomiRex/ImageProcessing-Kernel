2de5e6be8a83c31bd20b1641c2f5533a608ce042
Shlomi <vgtvgy1@gmail.com>
2020-08-23 16:29:39 +0300

    second commit - working ampt, freq, phase

--- main.py
+++ main.py
@@ -8,8 +8,9 @@ N = 256
 freq = 8.0
 ampt = 127.0
 phase = 0.0
+vec = 1
 
-def create_image(N, freq, ampt, phase):
+def create_image(N, freq, ampt, phase, vec):
     x = np.linspace(-np.pi, np.pi, N)
 
     sine1D = N / 2.0 + (ampt * np.sin((x * freq) + phase))
@@ -21,7 +22,7 @@ def create_image(N, freq, ampt, phase):
     return sine2D
 
 
-sine2D = create_image(N, freq, ampt, phase)
+sine2D = create_image(N, freq, ampt, phase, vec)
 fig, ax = plt.subplots()
 im = plt.imshow(sine2D, cmap='gray')
 
@@ -29,29 +30,38 @@ fig.subplots_adjust(bottom=0.2) # or whatever
 
 axamp = plt.axes([0.25, 0.1, 0.50, 0.02])
 slider_freq = Slider(axamp, 'Frequency', 0, 30, valinit=freq)
-axamp = plt.axes([0.25, 0.05, 0.50, 0.02])
-slider_ampt = Slider(axamp, 'Amptitude', 0, 255, valinit=ampt)
-axamp = plt.axes([0.25, 0.005, 0.50, 0.02])
+axamp = plt.axes([0.25, 0.07, 0.50, 0.02])
+slider_ampt = Slider(axamp, 'Amptitude', 0, ampt, valinit=ampt)
+axamp = plt.axes([0.25, 0.04, 0.50, 0.02])
 slider_phase = Slider(axamp, 'Phase', 0, 360, valinit=phase)
+axamp = plt.axes([0.25, 0.01, 0.50, 0.02])
+slider_vector = Slider(axamp, 'Vector', 0, 10, valinit=phase)
+
 
 def slider_freq_onchange(val):
     freq = val
-    sine2D = create_image(N, freq, ampt, phase)
+    sine2D = create_image(N, freq, ampt, phase, vec)
     im.set_array(sine2D)
 
 def slider_ampt_onchange(val):
     ampt = val
-    sine2D = create_image(N, freq, ampt, phase)
+    sine2D = create_image(N, freq, ampt, phase, vec)
     im.set_array(sine2D)
 
 def slider_phase_onchange(val):
     phase = val
-    sine2D = create_image(N, freq, ampt, phase)
+    sine2D = create_image(N, freq, ampt, phase, vec)
+    im.set_array(sine2D)
+
+def slider_vector_onchange(val):
+    vec = int(val)
+    sine2D = create_image(N, freq, ampt, phase, vec)
     im.set_array(sine2D)
 
 slider_freq.on_changed(slider_freq_onchange)
 slider_ampt.on_changed(slider_ampt_onchange)
 slider_phase.on_changed(slider_phase_onchange)
+slider_vector.on_changed(slider_vector_onchange)
 
 plt.show()
 
